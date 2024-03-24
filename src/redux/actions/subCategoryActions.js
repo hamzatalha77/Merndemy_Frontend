a23fc94a1836a288e4d460991f203e3e4a7a45bb
hamzatalha77 <hamza_ta7@hotmail.com>
2024-03-24 13:45:03 +0000

    commit

--- src/redux/actions/subCategoryActions.js
+++ src/redux/actions/subCategoryActions.js
@@ -5,34 +5,7 @@ import {
   SUBCATEGORY_LIST_SUCCESS
 } from '../constants/subCategoryConstants'
 import { BASE_URL } from '../../constants'
-import { logout } from './userActions'
 
-export const listSubCategories = () => async (dispatch) => {
-  try {
-    dispatch({
-      type: SUBCATEGORY_LIST_REQUEST
-    })
-
-    const { data } = await axios.get(`${BASE_URL}/api/subCategories/`)
-
-    dispatch({
-      type: SUBCATEGORY_LIST_SUCCESS,
-      payload: data
-    })
-  } catch (error) {
-    const message =
-      error.response && error.response.data.message
-        ? error.response.data.message
-        : error.message
-    if (message === 'Not authorized, token failed') {
-      dispatch(logout())
-    }
-    dispatch({
-      type: SUBCATEGORY_LIST_FAIL,
-      payload: message
-    })
-  }
-}
 export const listSubCategoriesForCategory =
   (categoryId) => async (dispatch) => {
     try {
@@ -58,3 +31,32 @@ export const listSubCategoriesForCategory =
       })
     }
   }
+
+// import { logout } from './userActions'
+
+// export const listSubCategories = () => async (dispatch) => {
+//   try {
+//     dispatch({
+//       type: SUBCATEGORY_LIST_REQUEST
+//     })
+
+//     const { data } = await axios.get(`${BASE_URL}/api/subCategories/`)
+
+//     dispatch({
+//       type: SUBCATEGORY_LIST_SUCCESS,
+//       payload: data
+//     })
+//   } catch (error) {
+//     const message =
+//       error.response && error.response.data.message
+//         ? error.response.data.message
+//         : error.message
+//     if (message === 'Not authorized, token failed') {
+//       dispatch(logout())
+//     }
+//     dispatch({
+//       type: SUBCATEGORY_LIST_FAIL,
+//       payload: message
+//     })
+//   }
+// }
