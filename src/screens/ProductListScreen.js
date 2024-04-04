89db4f7fef52035e2be77896c00f321490b68a8f
hamzatalha77 <hamza_ta7@hotmail.com>
2024-04-04 20:00:28 +0000

    commit

--- src/screens/ProductListScreen.js
+++ src/screens/ProductListScreen.js
@@ -92,7 +92,7 @@ const ProductListScreen = ({ history, match }) => {
                   <td>{product._id} </td>
                   <td>{product.name} </td>
                   <td>${product.price} </td>
-                  <td>{product.categories}</td>
+                  <td>{product.category}</td>
                   <td>{product.brand}</td>
                   <td>
                     <LinkContainer to={`/admin/product/${product._id}/edit`}>
